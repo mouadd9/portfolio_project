785522dc4b82c8a5a8a080bbadc8ee111333800d
Mouad Bencaid <bencaidmouad9@gmail.com>
2024-04-23 23:13:10 +0100

    test

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 const openButton = document.getElementById('idOpen');
 const closeButton = document.getElementById('idClose');
 const menu = document.querySelector('.menu');
-let b = 0;
-openButton.addEventListener('click',function (){
-    menu.style.transition = 'transform 0.5s ease-in-out'
+const menuLinks = document.querySelectorAll('.links .item .link'); // Select all menu links
+
+let isMenuOpen = false;
+
+function openMenu() {
+    menu.style.transition = 'transform 0.5s ease-in-out';
     menu.style.transform = 'translate(0, 0)';
-    b = 1;
-});
-closeButton.addEventListener('click',function (){
+    isMenuOpen = true;
+}
+
+function closeMenu() {
     menu.style.transition = 'transform 0.5s ease-in-out';
     menu.style.transform = 'translate(100vw, 0)';
-    b=1;
+    isMenuOpen = false;
+}
+
+openButton.addEventListener('click', openMenu);
+closeButton.addEventListener('click', closeMenu);
+
+// Close the menu when a link is clicked
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (isMenuOpen && window.innerWidth < 800) { // Checks if the menu is open and the screen width is less than 800px
+            closeMenu();
+        }
+    });
 });
+
 window.addEventListener('resize', function () {
-    if (window.innerWidth > 800 && b === 1) {
-        menu.removeAttribute('style'); 
+    if (window.innerWidth > 800 && isMenuOpen) {
+        menu.removeAttribute('style');
+        isMenuOpen = false; // Reset the menu state
     }
 });
